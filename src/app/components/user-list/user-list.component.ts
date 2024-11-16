3c2ef93d94f47ab3a23a65c77492c5501b960175
TanatosGeek <21160787@itoaxaca.edu.mx>
2024-11-15 22:51:33 -0600

    Uso de API

---
+++ src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,53 @@
+import { AfterViewInit, Component, OnInit, viewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { UserService } from '../../services/user.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [
+    MatPaginator,MatSort,
+    MatTableModule,MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+  ],
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'email', 'password' , 'role', 'avatar'];
+  dataSource = new MatTableDataSource<any>([]); // Inicializa con un arreglo vacío
+
+  readonly paginator = viewChild.required(MatPaginator);
+  readonly sort = viewChild.required(MatSort);
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    // Llama al servicio para obtener los usuarios y asignarlos al dataSource
+    this.userService.getUsers().subscribe((data: any[]) => {
+      this.dataSource.data = data; // Asigna los datos obtenidos a dataSource
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator();
+    this.dataSource.sort = this.sort();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
